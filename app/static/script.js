35a70c4302f6cea109ca30e35b648b3f6fccbe04
Pratik Choudhary <Pratik Choudhary>
2016-09-21 20:13:10 -0400

    second commit

--- app/static/script.js
+++ app/static/script.js
@@ -4,6 +4,23 @@
 
 $(document).ready(function () {
     $('#gomoku td').click(function () {
-        $(this).addClass('bluebg')
+        $(this).addClass('bluebg');
     })
 })
+
+
+function countDown(secs,elem) {
+	var element = document.getElementById(elem);
+	element.innerHTML = '<h4 style="font-family: serif; font-size: 20px;">You have <b>'+secs+'</b> seconds to play your move</h4>';
+
+
+    if (secs <= 0){
+        clearTimeout(timer);
+        alert("User has timed out. Start a New Game.");
+        return;
+    }
+    else {
+        secs--;
+    }
+	var timer = setTimeout('countDown('+secs+',"'+elem+'")',1000);
+}
